53704d387ae593ec6ff627237cebf6ed0efea85d
lordbron <noreply>
2016-01-12 18:16:37 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -10,6 +10,7 @@ var Component = require("montage/ui/component").Component;
 exports.Main = Component.specialize(/** @lends Main# */ {
     templateDidLoad: {
         value: function (event) {
+            this.templateObjects.buttonNoHTMLClassDisabledJSStyled.enabled = false;
             this.templateObjects.buttonNoHTMLClassDisabledJSStyled.element.classList = [];
             this.templateObjects.buttonNoHTMLClassDisabledJSStyled.element.className = "btn-primary-disabled";
         }
